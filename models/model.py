0b34d69b214e65673b41d15c89f9e27e2c012e1d
Picard1701ent <129656311+Picard1701ent@users.noreply.github.com>
2025-06-19 16:54:20 +0800

    Update model.py

--- models/model.py
+++ models/model.py
@@ -14,21 +14,26 @@ cfg = SimpleNamespace(**vars(config))
 class TAHGNN(nn.Module):
     def __init__(self):
         super().__init__()
-        self.ahgnn0 = TAHGConv(cfg.in_features, cfg.hidden_features, cfg.dropout)
-        self.ahgnn1 = TAHGConv(cfg.hidden_features, cfg.out_features, cfg.dropout)
+        self.layers = nn.ModuleList()
+        in_features = cfg.in_features
+        for i in range(cfg.layers - 1):
+            self.layers.append(TAHGConv(in_features, cfg.hidden_features, cfg.dropout))
+            in_features = cfg.hidden_features
+        self.layers.append(TAHGConv(in_features, cfg.out_features, cfg.dropout))
         self.classifier = Mlp(cfg.node_num * cfg.out_features, 256, 2, cfg.dropout)
 
     def forward(self, x, H, node_strength, gloable_efficiency, dist_matrix):
-        ahgnn_out0, _ = self.ahgnn0(x, H, node_strength, gloable_efficiency)
-        ahgnn_out0 = ahgnn_out0 + x
-        ahgnn_out1, e_fts = self.ahgnn1(
-            ahgnn_out0, H, node_strength, gloable_efficiency
-        )
-        ahgnn_out1 = ahgnn_out0 + ahgnn_out1
+        out = x
+        for i in range(cfg.layers - 1):
+            out, _ = self.layers[i](out, H, node_strength, gloable_efficiency)
+            out = out + x
+        out, e_fts = self.layers[-1](out, H, node_strength, gloable_efficiency)
+        out = out + x
 
-        ahgnn_out1 = torch.flatten(ahgnn_out1, start_dim=-2, end_dim=-1)
+        out = torch.flatten(out, start_dim=-2, end_dim=-1)
 
-        out = self.classifier(ahgnn_out1)
+        out = self.classifier(out)
         out = nn.functional.log_softmax(out, dim=-1)
 
         return out, e_fts
+
